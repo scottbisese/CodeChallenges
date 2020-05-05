3547148326464f683f5d03fe1e60766834716a3c
scottbisese <bisese.scott@gmail.com>
2020-05-04 20:38:34 -0500

    changed git init

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Challenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayReversed();
+        }
+        static void ArrayReversed()
+        {
+            int[] array1 = { 1, 2, 3 };
+            int[] array2 = new int[3];
+            int counter = 0;
+            for (int i = array1.Length - 1; i >= 0; i--)
+            {
+                array2[counter] = array1[i];
+                counter++;
+            }
+            Console.Write(string.Join(",", array2));
+        }
+    }
+}
+
